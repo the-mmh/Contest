5652fbde4611b76cbe97bd922bb91ebeae27f1c3
Piyush Gupta <52541720+piyushg9794@users.noreply.github.com>
2020-09-23 17:23:40 +0530

    deploy fix

--- app.js
+++ app.js
@@ -94,4 +94,4 @@ app.use((req, res, next) => {
 
 
 
-app.listen(5000, () => console.log('Server started on 5000!'));
+app.listen(process.env.PORT || 5000, () => console.log('Server started on 5000!'));
